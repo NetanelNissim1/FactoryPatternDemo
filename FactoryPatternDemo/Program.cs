bc47449fd172db64bcd39486bb463e69d9dda2fd
Nati <nati.nissim@gmail.com>
2017-11-22 12:35:28 +0200

    first

--- FactoryPatternDemo/Program.cs
+++ FactoryPatternDemo/Program.cs
@@ -10,6 +10,15 @@ namespace FactoryPatternDemo
     {
         static void Main(string[] args)
         {
+            var shapeFactory = new ShapeFactory();
+            Shape shape1 = shapeFactory.getShape("CIRCLE");
+            shape1.draw();
+
+            Shape shape2 = shapeFactory.getShape("RECTANGLE");
+            shape2.draw();
+
+            Shape shape3 = shapeFactory.getShape("SQUARE"); 
+            shape3.draw();
         }
     }
 }
---
+++ FactoryPatternDemo/ShapeFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternDemo
+{
+   public class ShapeFactory
+    {
+        public Shape getShape(string shapeType)
+        {
+            if (shapeType == null) return null;
+            if (shapeType.ToUpper().Equals("CIRCLE")) return new Circle();
+            if (shapeType.ToUpper().Equals("RECTANGLE")) return new Rectangle();
+            if (shapeType.ToUpper().Equals("SQUARE")) return new Square();
+            return null;
+        }
+    }
+}
